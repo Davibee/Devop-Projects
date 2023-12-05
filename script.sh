1cbb3cbc5533063d1d10300dfe8b4cdc52b2888b
Ubuntu <ubuntu@ip-172-31-24-117.eu-north-1.compute.internal>
2023-12-05 14:24:16 +0000

    second update

--- script.sh
+++ script.sh
@@ -12,8 +12,6 @@ set -e #Exits script when there is an error
 
 set -o #Used to exit script when there is a pipefail error
 
-sdhfks | echo | kdhsohd
- 
 df -h  #fnd the details about the disk space
 
 free #Find details of memory
