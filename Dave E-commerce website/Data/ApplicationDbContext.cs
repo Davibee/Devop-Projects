03050424c83c2b185d1cb305120dfd28b7d7fc32
David Oyinloye <davido@Davids-MacBook-Pro.local>
2023-12-19 21:58:23 +0000

    Initial commit

---
+++ Dave E-commerce website/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dave_E_commerce_website.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
---
+++ Dave E-commerce website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Dave_E_commerce_website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
